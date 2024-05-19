381519ef9b183ff8bd1bd4e94f2faf1eb0f81a8c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-05-19 15:41:51 +0900

    naosi

--- vector/7.cpp
+++ vector/7.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-vector<int>x_direction = {1,-1,0,0,-1,-1,1,1};
-vector<int>y_direction = {0,0,-1,1,-1,1,-1,1};
+int x_direction[] = {1,-1,0,0,-1,-1,1,1};
+int y_direction[] = {0,0,-1,1,-1,1,-1,1};
 
 // 自身が死んでいるかつ x=3 ならば生きたマスとなる。
 // 自身が生きているかつ x=2 or 3 ならば生き続ける。
@@ -24,7 +24,7 @@ int ft_count_dedline(int i , int j,int n, vector<vector<int>> *grid)
         int ny = j + y_direction[d];
         if(nx < 0 || nx >= n || ny < 0 || ny >= n)
             continue;
-        if(grid[nx][ny] == 1)
+        if((*grid)[nx][ny] == 1)
             count++;   
     }
     return (count);
@@ -32,17 +32,17 @@ int ft_count_dedline(int i , int j,int n, vector<vector<int>> *grid)
 
 void ft_case_deal(int i, int j,vector<vector<int>> *grid , int dead_line)
 {
-    if(grid[i][j] == 0 && dead_line == 3)
-        grid[i][j] = 1;
-    else if(grid[i][j] == 1 && ((dead_line == 2) || (dead_line == 3)))
-        grid[i][j] = 1;
-    else if(grid[i][j] == 1 && dead_line <= 1)
-        grid[i][j] = 0;
-    else if(grid[i][j] == 1 && dead_line >= 4)
-        grid[i][j] = 0;
+    if((*grid)[i][j] == 0 && dead_line == 3)
+        (*grid)[i][j] = 1;
+    else if((*grid)[i][j]== 1 && ((dead_line == 2) || (dead_line == 3)))
+        (*grid)[i][j] = 1;
+    else if((*grid)[i][j] == 1 && dead_line <= 1)
+        (*grid)[i][j] = 0;
+    else if((*grid)[i][j] == 1 && dead_line >= 4)
+        (*grid)[i][j] = 0;
 }
 
-void *ft_solove_life_game(vector<vector<int>> *grid , int n , int x)
+void ft_solove_life_game(vector<vector<int>> *grid , int n , int x)
 {
     int dead_line;
     int step;
@@ -55,8 +55,8 @@ void *ft_solove_life_game(vector<vector<int>> *grid , int n , int x)
         {
             for(int j = 0; j < n; j++)
             {
-                int dead_line = ft_count_dedline(i , j , n, &grid);
-                ft_case_deal(i , j , &grid , dead_line);
+                int dead_line = ft_count_dedline(i , j , n, grid);
+                ft_case_deal(i , j , grid , dead_line);
             }
         }
         x--;;  
